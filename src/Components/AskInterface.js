0527339e5283a0773ff47a7ad0689192c0f6f120
Swarnab Garang <72680953+0xcatrovacer@users.noreply.github.com>
2022-06-13 19:03:35 +0530

    restructure: Rewired component to show selected NFT

--- src/Components/AskInterface.js
+++ src/Components/AskInterface.js
@@ -1,16 +1,26 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 function AskInterface({ selectedNft }) {
+    const [imgLink, setImgLink] = useState(null);
+
+    const getNftImage = async () => {
+        const nft_metadata = await axios.get(selectedNft.data.uri);
+
+        setImgLink(nft_metadata.data.image);
+    };
+
     const SelectedNftHandler = () => {
         return (
             <div className="nft-ask-inner">
-                <div className="nft-ask-data">
-                    <img
-                        src={selectedNft.data.uri}
-                        className="selected-nft-img"
-                    />
-                    <div className="nft-ask-name">{selectedNft.data.name}</div>
-                </div>
+                {selectedNft.data && (
+                    <div className="nft-ask-data">
+                        <img src={imgLink} className="selected-nft-img" />
+                        <div className="nft-ask-name">
+                            {selectedNft.data.name}
+                        </div>
+                    </div>
+                )}
                 <div className="nft-ask-form">
                     <input
                         placeholder="0.00"
@@ -28,13 +38,17 @@ function AskInterface({ selectedNft }) {
         );
     };
 
+    useEffect(() => {
+        selectedNft && getNftImage();
+    }, [selectedNft]);
+
     return (
         <div className="ask-interface">
             <div className="ask-interface-head">Quote a Price</div>
             {selectedNft ? (
                 <SelectedNftHandler />
             ) : (
-                <div>Please Select and NFT</div>
+                <div className="no-nft-selected">Please Select and NFT</div>
             )}
         </div>
     );
