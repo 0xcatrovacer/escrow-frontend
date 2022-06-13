332c5210fa440c30eee4e839f836ef02f1051757
Swarnab Garang <72680953+0xcatrovacer@users.noreply.github.com>
2022-06-14 00:48:13 +0530

    feat: Added rpc call to initializeEscrow

--- src/Components/AskInterface.js
+++ src/Components/AskInterface.js
@@ -1,8 +1,21 @@
+import { PublicKey } from "@solana/web3.js";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { BN, web3 } from "@project-serum/anchor";
+import { getAssociatedTokenAddress, TOKEN_PROGRAM_ID } from "@solana/spl-token";
 
-function AskInterface({ selectedNft }) {
+function AskInterface({
+    selectedNft,
+    publicKey,
+    connection,
+    program,
+    provider,
+}) {
+    const [ask, setAsk] = useState("");
     const [imgLink, setImgLink] = useState(null);
+
+    const usdcMint = new PublicKey(process.env.REACT_APP_USDC_MINT);
+    const usdcDecimals = 1000000;
 
     const getNftImage = async () => {
         const nft_metadata = await axios.get(selectedNft.data.uri);
@@ -10,32 +23,58 @@ function AskInterface({ selectedNft }) {
         setImgLink(nft_metadata.data.image);
     };
 
-    const SelectedNftHandler = () => {
-        return (
-            <div className="nft-ask-inner">
-                {selectedNft.data && (
-                    <div className="nft-ask-data">
-                        <img src={imgLink} className="selected-nft-img" />
-                        <div className="nft-ask-name">
-                            {selectedNft.data.name}
-                        </div>
-                    </div>
-                )}
-                <div className="nft-ask-form">
-                    <input
-                        placeholder="0.00"
-                        type={"number"}
-                        className="price-input"
-                    />
-                    <div className="ask-usdc">USDC</div>
-                </div>
-                <div className="create-listing">
-                    <button className="create-listing-button">
-                        Create Listing
-                    </button>
-                </div>
-            </div>
-        );
+    const handleCreateListing = async (e) => {
+        e.preventDefault();
+
+        const escrowAccount = web3.Keypair.generate();
+
+        const nftMint = new PublicKey(selectedNft.mint);
+
+        try {
+            const [vault_account_pda, _vault_account_bump] =
+                await PublicKey.findProgramAddress(
+                    [publicKey.toBuffer(), nftMint.toBuffer()],
+                    program.programId
+                );
+
+            const initializerAssociatedNftAccount =
+                await getAssociatedTokenAddress(nftMint, publicKey);
+
+            const initializerAssociatedReceiveAccount =
+                await getAssociatedTokenAddress(usdcMint, publicKey);
+
+            await program.methods
+                .initializeEscrow(new BN(ask * usdcDecimals))
+                .accounts({
+                    vaultAccount: vault_account_pda,
+                    escrowAccount: escrowAccount.publicKey,
+                    initializer: publicKey,
+                    initializerNftMint: nftMint,
+                    initializerDepositTokenAccount:
+                        initializerAssociatedNftAccount,
+                    initializerReceiveMint: usdcMint,
+                    initializerReceiveTokenAccount:
+                        initializerAssociatedReceiveAccount,
+                    tokenProgram: TOKEN_PROGRAM_ID,
+                    rent: web3.SYSVAR_RENT_PUBKEY,
+                    systemProgram: web3.SystemProgram.programId,
+                })
+                .preInstructions([
+                    await program.account.escrowAccount.createInstruction(
+                        escrowAccount
+                    ),
+                ])
+                .signers([escrowAccount])
+                .rpc();
+
+            let _escrowAccount = await program.account.escrowAccount.fetch(
+                escrowAccount.publicKey
+            );
+
+            console.log(_escrowAccount);
+        } catch (e) {
+            console.log(e);
+        }
     };
 
     useEffect(() => {
@@ -45,9 +84,42 @@ function AskInterface({ selectedNft }) {
     return (
         <div className="ask-interface">
             <div className="ask-interface-head">Quote a Price</div>
-            {selectedNft ? (
-                <SelectedNftHandler />
-            ) : (
+            {selectedNft && (
+                <div className="nft-ask-inner">
+                    {selectedNft.data && (
+                        <div className="nft-ask-data">
+                            <img src={imgLink} className="selected-nft-img" />
+                            <div className="nft-ask-name">
+                                {selectedNft.data.name}
+                            </div>
+                        </div>
+                    )}
+                    <div className="nft-ask-form">
+                        <input
+                            name="ask"
+                            placeholder="0.00"
+                            type="number"
+                            className="price-input"
+                            min={0}
+                            value={ask}
+                            required
+                            onChange={(e) => {
+                                setAsk(e.target.value);
+                            }}
+                        />
+                        <div className="ask-usdc">USDC</div>
+                    </div>
+                    <div className="create-listing">
+                        <button
+                            className="create-listing-button"
+                            onClick={handleCreateListing}
+                        >
+                            Create Listing
+                        </button>
+                    </div>
+                </div>
+            )}
+            {!selectedNft && (
                 <div className="no-nft-selected">Please Select and NFT</div>
             )}
         </div>
