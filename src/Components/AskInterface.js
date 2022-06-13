b64a788e1b7258b227dbdad5e11e67c7384de7e3
Swarnab Garang <72680953+0xcatrovacer@users.noreply.github.com>
2022-06-13 14:03:48 +0530

    feat: Added AskInterface component

---
+++ src/Components/AskInterface.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+function AskInterface({ selectedNft }) {
+    const SelectedNftHandler = () => {
+        return (
+            <div className="nft-ask-inner">
+                <div className="nft-ask-data">
+                    <img src={selectedNft.img} className="selected-nft-img" />
+                    <div className="nft-ask-name">{selectedNft.name}</div>
+                </div>
+                <div className="nft-ask-form">
+                    <input
+                        placeholder="0.00"
+                        type={"number"}
+                        className="price-input"
+                    />
+                    <div className="ask-usdc">USDC</div>
+                </div>
+                <div className="create-listing">
+                    <button className="create-listing-button">
+                        Create Listing
+                    </button>
+                </div>
+            </div>
+        );
+    };
+
+    return (
+        <div className="ask-interface">
+            <div className="ask-interface-head">Quote a Price</div>
+            {selectedNft ? (
+                <SelectedNftHandler />
+            ) : (
+                <div>Please Select and NFT</div>
+            )}
+        </div>
+    );
+}
+
+export default AskInterface;
