13fe01f21928d1463882eb6074cd00e8700db1b0
Swarnab Garang <72680953+0xcatrovacer@users.noreply.github.com>
2022-06-14 11:20:42 +0530

    fix: Resolved minor errors

--- src/Components/HomeNfts.js
+++ src/Components/HomeNfts.js
@@ -3,31 +3,7 @@ import {
     resolveToWalletAddress,
     getParsedNftAccountsByOwner,
 } from "@nfteyez/sol-rayz";
-import axios from "axios";
 import Nft from "./Nft";
-
-const tempUserNFTs = [
-    {
-        img: "https://www.arweave.net/PShUK1uSCVszk8XfY-hLaiXhoxT91_Qmsz_FvbKzWLo?ext=jpg",
-        name: "Degen Ape #4269",
-    },
-    {
-        img: "https://arweave.net/2LBuacRI_sVHbEJwLz_jh9m116smpJeArx01vYep7Xk",
-        name: "Degen Ape #4270",
-    },
-    {
-        img: "https://www.degenape.academy/images/framed_apes/14.jpg",
-        name: "Degen Ape #4271",
-    },
-    {
-        img: "https://www.degenape.academy/images/framed_apes/33.jpg",
-        name: "Degen Ape #4272",
-    },
-    {
-        img: "https://arweave.net/12oSAVNB-zbBDLaejqRUIyOGSgkogrgZeI-g7uFaPaw",
-        name: "Degen Ape #4273",
-    },
-];
 
 function HomeNfts({ setSelectedNft, publicKey, connection }) {
     const [userNfts, setUserNfts] = useState(null);
