a7adb99eb649ed7e4724fb3d5633480c1068c980
Swarnab Garang <72680953+0xcatrovacer@users.noreply.github.com>
2022-06-14 11:22:25 +0530

    restructure: Rewired ListingPage to use Listing component

--- src/pages/ListingsPage.js
+++ src/pages/ListingsPage.js
@@ -1,7 +1,68 @@
-import React from "react";
+import {
+    getParsedAccountByMint,
+    getParsedNftAccountsByOwner,
+} from "@nfteyez/sol-rayz";
+import { useWallet } from "@solana/wallet-adapter-react";
+import React, { useEffect, useState } from "react";
+import Listing from "../Components/Listing";
 
-function ListingsPage() {
-    return <div>ListingsPage</div>;
+import "./ListingsPage.css";
+
+function ListingsPage({ setPublicKey, connection, program, provider }) {
+    const [nftListings, setListings] = useState([]);
+    const [parsedNfts, setParsedNfts] = useState([]);
+
+    const { publicKey } = useWallet();
+
+    const getAllListings = async () => {
+        const listings = await program.account.escrowAccount.all([
+            {
+                memcmp: {
+                    offset: 8,
+                    bytes: publicKey.toBase58(),
+                },
+            },
+        ]);
+
+        const parsedAccount = await getParsedAccountByMint({
+            mintAddress: listings[0].account.initializerNftMint.toBase58(),
+            connection,
+        });
+
+        const nfts = await getParsedNftAccountsByOwner({
+            publicAddress: parsedAccount.account.data.parsed.info.owner,
+            connection,
+        });
+
+        console.log(nfts);
+
+        setListings(listings);
+        setParsedNfts(nfts);
+    };
+
+    useEffect(() => {
+        setPublicKey(publicKey);
+        getAllListings();
+    }, []);
+
+    return (
+        <div>
+            {(nftListings.length == 0 || parsedNfts.length == 0) && (
+                <div>No Listings Available</div>
+            )}
+            <div className="all-listings">
+                {nftListings &&
+                    parsedNfts.map((nft, index) => (
+                        <Listing
+                            nft={nft}
+                            listing={nftListings[index]}
+                            connection={connection}
+                            key={nft.mint}
+                        />
+                    ))}
+            </div>
+        </div>
+    );
 }
 
 export default ListingsPage;
